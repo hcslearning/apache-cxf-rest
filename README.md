0add4816b79d8114ee47c4744b4fa016a2875a61
hcslearning <learning@hardcybersoft.cl>
2025-08-09 01:02:12 -0400

    README con pruebas sobre la API

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
 # Apache CXF Rest
 
+Proyecto básico de ejemplo de Jakarta RESTful Web Services 3.1 usando Apache CXF. 
+
+## Tecnologías 
+
+- Java 21
+- Apache CXF
+- Jakarta RESTful Web Services 3.1
+- Gradle 
+
+## Ejecución
+
+Para iniciar el proyecto:
+
+```bash
+./gradlew run 
+```
+
+## Pruebas 
+
+### Recupera todos los productos
+```
+curl -i -X GET http://localhost:9000/api/producto
+```
+
+### Recupera un producto por ID
+```
+curl -i -X GET http://localhost:9000/api/producto/2
+```
+
+### Agrega un producto nuevo
+
+```
+curl -i -X POST http://localhost:9000/api/producto \
+  -H "Content-Type: application/json" \
+  -d '{
+        "id": 4,
+        "nombre": "Cinturón de cuero",
+        "precio": 12990
+      }'
+
+```
+
+### Edita un producto
+
+```
+curl -i -X PUT http://localhost:9000/api/producto/4 \
+  -H "Content-Type: application/json" \
+  -d '{
+        "id": 4,
+        "nombre": "Cinturón de cuero genuino",
+        "precio": 14990
+      }'
+```
+
+### Elimina un producto
+
+```
+curl -i -X DELETE http://localhost:9000/api/producto/4
+```
 
--- app/src/main/java/cl/hcslearning/cxf/rest/ProductoService.java
+++ app/src/main/java/cl/hcslearning/cxf/rest/ProductoService.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,11 +12,11 @@ import java.util.Optional;
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductoService {
 
-    private final List<Producto> productos = List.of(
+    private static final List<Producto> productos = new ArrayList<>( List.of(
             new Producto(1, "Plato comida metálico para mascotas", 5_990),
             new Producto(2, "Balón fútbol", 19_990),
             new Producto(3, "Fundas de Asientos 8 piezas", 49_990)
-    );
+    ));
 
     @GET
     @Path("/{id}")
@@ -36,4 +37,35 @@ public class ProductoService {
     public List<Producto> getProductos() {
         return productos;
     }
+
+    @POST
+    public Response addProducto(Producto nuevo) {
+        boolean existe = productos.stream().anyMatch(p -> p.getId() == nuevo.getId());
+        if (existe) {
+            throw new WebApplicationException("Ya existe un producto con ese ID", Response.Status.CONFLICT);
+        }
+        productos.add(nuevo);
+        return Response.status(Response.Status.CREATED).entity(nuevo).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Producto updateProducto(@PathParam("id") long id, Producto actualizado) {
+        Producto existente = getProducto(id);
+        existente.setNombre(actualizado.getNombre());
+        existente.setPrecio(actualizado.getPrecio());
+        return existente;
+    }
+
+    // Eliminar un producto
+    @DELETE
+    @Path("/{id}")
+    public Response deleteProducto(@PathParam("id") long id) {
+        boolean eliminado = productos.removeIf(p -> p.getId() == id);
+        if (!eliminado) {
+            throw new WebApplicationException("Producto no encontrado", Response.Status.NOT_FOUND);
+        }
+        return Response.noContent().build(); // http response 204
+    }
+
 }
