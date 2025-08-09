2fb711d7d3bfd293d0d0feda6c6d2fcfd500a1db
hcslearning <learning@hardcybersoft.cl>
2025-08-09 00:17:54 -0400

    mejoras en servicio rest

--- app/src/main/java/cl/hcslearning/cxf/rest/App.java
+++ app/src/main/java/cl/hcslearning/cxf/rest/App.java
@@ -4,12 +4,14 @@ import jakarta.ws.rs.SeBootstrap;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
-
 public class App {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
+        jakartaRsWsStart();
+        // cxfStart();
+    }
+
+    private static void jakartaRsWsStart() {
         final SeBootstrap.Configuration conf = SeBootstrap.Configuration
                 .builder()
                 .protocol("http")
@@ -19,34 +21,18 @@ public class App {
                 .build()
         ;
 
-        SeBootstrap.Instance instance = SeBootstrap
+        SeBootstrap
                 .start(new MiAPI(), conf)
                 .toCompletableFuture()
-                .get()
+                .join()
         ;
-
-        // Latch para bloquear el main
-        CountDownLatch latch = new CountDownLatch(1);
-
-        // Se ejecuta cuando llega señal de apagado (Ctrl+C)
-        instance.stopOnShutdown(inst -> {
-            System.out.println("Servidor detenido");
-            latch.countDown();
-        });
-
-        // Bloquea hasta que latch.countDown() sea llamado
-        latch.await();
     }
 
     private static void cxfStart() {
         JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
-
-        sf.setApplication( new MiAPI() );
-
-        //sf.setResourceClasses(ProductoService.class);
-        //sf.setResourceProvider(ProductoService.class,
-        //   new SingletonResourceProvider(new ProductoService()));
-
+        sf.setResourceClasses(ProductoService.class);
+        sf.setResourceProvider(ProductoService.class,
+        new SingletonResourceProvider(new ProductoService()));
         sf.setAddress("http://localhost:9000");
         sf.create();
     }
--- app/src/main/java/cl/hcslearning/cxf/rest/ProductoService.java
+++ app/src/main/java/cl/hcslearning/cxf/rest/ProductoService.java
@@ -1,19 +1,39 @@
 package cl.hcslearning.cxf.rest;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.Optional;
 
 @Path("/producto")
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductoService {
 
+    private final List<Producto> productos = List.of(
+            new Producto(1, "Plato comida metálico para mascotas", 5_990),
+            new Producto(2, "Balón fútbol", 19_990),
+            new Producto(3, "Fundas de Asientos 8 piezas", 49_990)
+    );
+
     @GET
     @Path("/{id}")
     public Producto getProducto(@PathParam("id") long id) {
-        return new Producto(1, "Plato comida metálico para mascotas", 5_990);
+        return productos
+                .stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseThrow(() ->
+                    new WebApplicationException(
+                            "Producto no encontrado",
+                            Response.Status.NOT_FOUND
+                    )
+                );
     }
 
+    @GET
+    public List<Producto> getProductos() {
+        return productos;
+    }
 }
